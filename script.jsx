9b42df8fb17883d30f17fd2df3181532a5fe4a8c
jshuadvd <jshuadvd@icloud.com>
2015-09-21 23:04:01 -0700

    Added the render function to the main component

--- script.jsx
+++ script.jsx
@@ -1,3 +1,13 @@
+var Game = React.createClass({
+render: function() {
+  return (
+    <div id="game">
+
+    </div>    
+  );
+}
+});
+
 React.render(
     <h1>Hello!</h1>,
     document.getElementById('container')
