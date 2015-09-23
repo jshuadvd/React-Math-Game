5b24dbe7dd8828852c40bd3eeecd82de4dd392f5
jshuadvd <jshuadvd@icloud.com>
2015-09-22 23:05:54 -0700

    Added two new components

--- script.jsx
+++ script.jsx
@@ -8,12 +8,33 @@ render: function() {
 }
 });
 
+var ButtonFrame = React.createClass({
+render: function() {
+  return (
+    <div id="">
+      ...
+    </div>
+  );
+}
+});
+
+var AnswerFrame = React.createClass({
+render: function() {
+  return (
+    <div id="">
+      ...
+    </div>
+  );
+}
+});
+
 var Game = React.createClass({
 render: function() {
   return (
     <div id="game">
       <h2>Play Nine</h2>
       <StarsFrame />
+
     </div>
   );
 }
