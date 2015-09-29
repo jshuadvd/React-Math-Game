e11a098f56a698d11d8d413b62925946693d7db7
jshuadvd <jshuadvd@icloud.com>
2015-09-28 23:38:25 -0700

    Added new numbers for the numbers frame

--- script.jsx
+++ script.jsx
@@ -1,76 +1,73 @@
 var StarsFrame = React.createClass({
-render: function() {
-  var numberOfStars = Math.floor(Math.random() * 9) + 1;
+  render: function() {
+    var numberOfStars = Math.floor(Math.random() * 9) + 1;
 
-  var stars = [];
-  for (var i = 0; i < numberOfStars; i++) {
-    stars.push(
-      <span className="glyphicon glyphicon-star"></span>
+    var stars = [];
+    for (var i = 0; i < numberOfStars; i++) {
+      stars.push(<span className="glyphicon glyphicon-star"></span>);
+    }
+
+    return (
+      <div id="stars-frame">
+        <div className="well">
+          {stars}
+        </div>
+      </div>
     );
   }
-
-  return (
-    <div id="stars-frame">
-      <div className="well">
-        {stars}
-      </div>
-    </div>
-  );
- }
 });
 
 var ButtonFrame = React.createClass({
-render: function() {
-  return (
-    <div id="button-frame">
-      <button className="btn btn-primary btn-lg">=</button>
-    </div>
-  );
- }
+  render: function() {
+    return (
+      <div id="button-frame">
+        <button className="btn btn-primary btn-lg">=</button>
+      </div>
+    );
+  }
 });
 
 var AnswerFrame = React.createClass({
-render: function() {
-  return (
-    <div id="answer-frame">
-      <div className="well"></div>
-    </div>
-  );
-}
+  render: function() {
+    return (
+      <div id="answer-frame">
+        <div className="well"></div>
+      </div>
+    );
+  }
 });
 
 var NumbersFrame = React.createClass({
-render: function() {
-  return (
-    <div id="answer-frame">
-      <div className="well"></div>
-    </div>
-  );
-}
+  render: function() {
+    return (
+      <div id="numbers-frame">
+        <div className="well">
+          <div className="number">4</div>
+          <div className="number">9</div>
+          <div className="number">6</div>
+        </div>
+      </div>
+    );
+  }
 });
 
 var Game = React.createClass({
-render: function() {
-  return (
-    <div id="game">
-      <h2>Play Nine</h2>
-      <hr />
-      <div className="clearfix">
-        <StarsFrame />
-        <ButtonFrame />
-        <AnswerFrame />
-      </div>
+  render: function() {
+    return (
+      <div id="game">
+        <h2>Play Nine</h2>
+        <hr/>
+        <div className="clearfix">
+          <StarsFrame/>
+          <ButtonFrame/>
+          <AnswerFrame/>
+        </div>
 
         <NumbersFrame/>
 
-
-    </div>
-  );
-}
+      </div>
+    );
+  }
 });
 
-
-React.render(
-    <Game />,
-    document.getElementById('container')
-  );
+React.render(<Game/>, document.getElementById('container'));
