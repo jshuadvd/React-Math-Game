c42b84b9560fcc27b7a1a42102386aa9dc29c0f1
jshuadvd <jshuadvd@icloud.com>
2015-12-23 22:23:55 -0800

    Adding a Button

--- script.jsx
+++ script.jsx
@@ -135,6 +135,7 @@ var DoneFrame = React.createClass({
     return (
       <div className="well text-center">
         <h2>{this.props.doneStatus}</h2>
+        <button></button>
       </div>
     )
   }
